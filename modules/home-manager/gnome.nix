2cc3630d439373be919d10953d91f13f04dd78a8
Mozart409 <amadeus@mozart409.com>
2025-03-04 23:07:59 +0100

    fix: :fire: remove kanagawa

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -6,9 +6,9 @@
 }:
 {
 
-  gnome-tweaks.enable = true;
-  kanagawa-gtk-theme.enable = true;
-  kanagawa-icon-theme.enable = true;
+  # gnome-tweaks.enable = true;
+  # kanagawa-gtk-theme.enable = true;
+  # kanagawa-icon-theme.enable = true;
   dconf = {
     enable = true;
     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
