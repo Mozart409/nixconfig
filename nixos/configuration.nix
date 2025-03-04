407011b92e7e283254bbb9835800dde33d8b3e74
Mozart409 <amadeus@mozart409.com>
2025-03-04 21:55:18 +0100

    fix: :wrench: added zsh.enable

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -139,6 +139,8 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
+  programs.zsh.enable = true;
+
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
 
