ad8076335ec0405ef0380eb6994944a6f58bcacc
Mozart409 <amadeus@mozart409.com>
2025-03-04 22:53:32 +0100

    fix: :fire: removed dconf

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -10,10 +10,10 @@
     gnome-tweaks.enable = true;
     kanagawa-gtk-theme.enable = true;
     kanagawa-icon-theme.enable = true;
-    dconf = {
-      enable = true;
-      settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-    };
+    # dconf = {
+    #   enable = true;
+    #   settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    # };
   };
 
 }
