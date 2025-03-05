35f38307f656fc8dd35b35d2b50557502edaeb6c
Mozart409 <amadeus@mozart409.com>
2025-03-05 11:55:46 +0100

    feat: :sparkles: added more buttons

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -12,6 +12,7 @@
   dconf = {
     enable = true;
     settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    settings."org/gnome/desktop/wm/preferences".button-layout = "minimize,maximize,close";
   };
 
 }
