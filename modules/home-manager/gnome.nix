1e2ce6204db57a5aa9c18150035c3922972d1c3d
Mozart409 <amadeus@mozart409.com>
2025-03-05 12:11:19 +0100

    feat: :sparkles: added more gnome settings

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -11,8 +11,33 @@
   # kanagawa-icon-theme.enable = true;
   dconf = {
     enable = true;
-    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
-    settings."org/gnome/desktop/wm/preferences".button-layout = "minimize,maximize,close";
+    settings = {
+      "org/gnome/shell/extensions/advanced-alt-tab-window-switcher" = {
+        app-switcher-popup-fav-apps = false;
+        app-switcher-popup-filter = 2;
+        app-switcher-popup-include-show-apps-icon = false;
+        app-switcher-popup-search-pref-running = true;
+        enable-super = false;
+        hot-edge-position = 0;
+        super-double-press-action = 3;
+        super-key-mode = 3;
+        switcher-popup-hot-keys = false;
+        switcher-popup-interactive-indicators = true;
+        switcher-popup-position = 3;
+        switcher-popup-timeout = 0;
+        switcher-ws-thumbnails = 2;
+        win-switcher-popup-filter = 2;
+        win-switcher-popup-order = 1;
+      };
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+        font-antialiasing = "grayscale";
+        font-hinting = "slight";
+
+      };
+
+      "org/gnome/desktop/wm/preferences".button-layout = "minimize,maximize,close";
+    };
   };
 
 }
