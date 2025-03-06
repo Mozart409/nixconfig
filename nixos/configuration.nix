8d0dbe6d09c08eafec899d90274a37bb102a38fb
Mozart409 <amadeus@mozart409.com>
2025-03-06 22:41:36 +0100

    fix: :wrench: enabled channel

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -63,8 +63,7 @@
         # Workaround for https://github.com/NixOS/nix/issues/9574
         nix-path = config.nix.nixPath;
       };
-      # Opinionated: disable channels
-      channel.enable = false;
+      channel.enable = true;
 
       # Opinionated: make flake registry and nix path match flake inputs
       registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
