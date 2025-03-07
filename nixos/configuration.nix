89b47790227e582f4bd6836e0a6723547d73a3d1
Mozart409 <amadeus@mozart409.com>
2025-03-07 23:38:36 +0100

    feat: :sparkles: added podman

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -226,6 +226,24 @@
     };
   };
 
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
+  # Useful other development tools
+  environment.systemPackages = with pkgs; [
+    dive # look into docker image layers
+    podman-tui # status of containers in the terminal
+    podman-compose # start group of containers for dev
+  ];
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "amadeus";
