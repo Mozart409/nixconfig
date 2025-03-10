040bfedaf1c3b58f5c9d4e403d1597063e2abf44
Mozart409 <amadeus@mozart409.com>
2025-03-10 12:08:42 +0100

    feat: :sparkles: changed keymaps

--- modules/home-manager/kickstart.nixvim/plugins/custom/plugins/lazygit.nix
+++ modules/home-manager/kickstart.nixvim/plugins/custom/plugins/lazygit.nix
@@ -3,7 +3,7 @@
     enable = true;
     keymaps = [
       {
-        key = "<leader>GG";
+        key = "<leader>tt";
         action = "<cmd>LazyGit<cr>";
         options = {
           desc = "LazyGit reveal";
