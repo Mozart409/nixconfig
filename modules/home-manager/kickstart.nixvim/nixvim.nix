0a557cbeff5392c3878b9bb6414084c807fc85e6
Mozart409 <amadeus@mozart409.com>
2025-05-04 22:36:29 +0200

    fix: :bug: moved keymaps to main file

--- modules/home-manager/kickstart.nixvim/nixvim.nix
+++ modules/home-manager/kickstart.nixvim/nixvim.nix
@@ -205,6 +205,53 @@
     #  See `:help vim.keymap.set()`
     # https://nix-community.github.io/nixvim/keymaps/index.html
     keymaps = [
+      # Keymaps for custom plugin trouble
+      {
+        mode = "";
+        key = "<leader>xx";
+        action = "<cmd>Trouble diagnostics toggle<cr>";
+        # action.__raw = ''
+        #   function()
+        #     require('Trouble').format { async = true, lsp_fallback = true }
+        #   end
+        # '';
+        options = {
+          desc = "Diagnostics (Trouble)";
+        };
+      }
+      {
+        mode = "";
+        key = "<leader>xX";
+        action = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+        options = {
+          desc = "Buffer Diagnostics (Trouble)";
+        };
+      }
+      {
+        mode = "";
+        key = "<leader>cs";
+        action = "<cmd>Trouble symbols toggle focus=false<cr>";
+        options = {
+          desc = "Symbols (Trouble)";
+        };
+      }
+      {
+        mode = "";
+        key = "<leader>xL";
+        action = "<cmd>Trouble loclist toggle<cr>";
+        options = {
+          desc = "Location List(Trouble)";
+        };
+      }
+      {
+        mode = "";
+        key = "<leader>xQ";
+        action = "<cmd>Trouble qflist toggle<cr>";
+        options = {
+          desc = "Quickfix List(Trouble)";
+        };
+      }
+
       # Clear highlights on search when pressing <Esc> in normal mode
       {
         mode = "n";
